941d436575371751da48eb1c754f91f426f50511
Buyaki01 <rittahbuyaki@gmail.com>
2020-05-31 19:41:33 +0300

    Test my_each_with_index method

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -3,8 +3,7 @@ require './enumerable_methods.rb'
 RSpec.describe Enumerable do
     let(:b) { [1, 3, 5, 7] }
     let(:c) { [1, 2, 4, 2] }
-    let(:d) { %w[(ant bear cat)] }
-    let(:e) { [] } 
+    let(:d) { %w[(ant bear cat)] } 
 
     describe '#my_each' do
       it 'if no block is given, it should return enumerator' do
@@ -13,6 +12,17 @@ RSpec.describe Enumerable do
 
       it 'if an array is given, it should return an array' do
         expect(b.my_each {|i| i}).to eql(b)
+      end
+    end
+
+    describe '#my_each_with_index' do
+      it 'if no block is given, it should return enumerator' do
+        expect(d.my_each).to be_a(Enumerator)
+      end
+
+      it 'Should return i and j if block is given' do
+        hash = {}
+        expect(d.my_each_with_index {|i,j| hash[i] = j }).to eql(d)
       end
     end
 end
